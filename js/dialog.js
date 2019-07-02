10a248dbbe8c06fe127a07690f7e299e7ab1ab45
ar78ilnik <arakchiev.ilya@yandex.ru>
2019-07-02 23:36:15 +0300

    Move on

--- js/dialog.js
+++ js/dialog.js
@@ -1,7 +1,11 @@
 'use strict';
 (function () {
-  var setup = document.querySelector('.setup');
-  var dialogHandler = setup.querySelector('.upload');
+
+  var TOP_DISTANCE = 80;
+  var LEFT_DISTANCE = 50;
+  var setupDialogElement = document.querySelector('.setup');
+  var dialogHandler = setupDialogElement.querySelector('.upload');
+  var closeDialogHandler = setupDialogElement.querySelector('.setup-close');
 
   dialogHandler.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
@@ -15,6 +19,7 @@
 
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
+      dragged = true;
 
       var shift = {
         x: startCoords.x - moveEvt.clientX,
@@ -26,22 +31,30 @@
         y: moveEvt.clientY
       };
 
-      setup.style.top = (setup.offsetTop - shift.y) + 'px';
-      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+      setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
+      setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
+
     };
+    closeDialogHandler.addEventListener('click', function () {
+      setupDialogElement.style.top = TOP_DISTANCE + 'px';
+      setupDialogElement.style.left = LEFT_DISTANCE + '%';
+    });
     var onMouseUp = function (upEvt) {
       upEvt.preventDefault();
+
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
 
       if (dragged) {
-        var onClickPreventDefault = function () {
-          evt.preventDefault();
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
           dialogHandler.removeEventListener('click', onClickPreventDefault);
         };
         dialogHandler.addEventListener('click', onClickPreventDefault);
       }
+
     };
+
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   });
